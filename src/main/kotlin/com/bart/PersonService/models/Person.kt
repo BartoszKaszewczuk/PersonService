a00fdf2cf667929fce455e45ec0902d556f95774
Raj <the.raj.saxena@gmail.com>
2017-10-13 13:22:43 +0200

    #3 Created PersonService Interface

---
+++ src/main/kotlin/com/bart/PersonService/models/Person.kt
@@ -0,0 +1,3 @@
+package com.bart.PersonService.models
+
+data class Person(val firstName: String, val lastName:String, val age:Int)
--- src/main/kotlin/com/bart/PersonService/services/PersonService.kt
+++ src/main/kotlin/com/bart/PersonService/services/PersonService.kt
@@ -1,4 +1,11 @@
 package com.bart.PersonService.services
 
+import com.bart.PersonService.models.Person
+import org.springframework.stereotype.Service
+
+@Service
 class PersonService {
+    fun getPersons(): List<Person> {
+        return emptyList()
+    }
 }
