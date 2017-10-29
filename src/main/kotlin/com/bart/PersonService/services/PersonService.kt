e25673314785b7a2a75253e3066ce054886d6359
jblejder <jakubblejder@gmail.com>
2017-10-29 10:04:08 +0100

    add stub methods to person service (#11)
    * add stub methods to person service
    
    * Extract PersonService interface

--- src/main/kotlin/com/bart/PersonService/services/PersonService.kt
+++ src/main/kotlin/com/bart/PersonService/services/PersonService.kt
@@ -1,11 +1,9 @@
 package com.bart.PersonService.services
 
 import com.bart.PersonService.models.Person
-import org.springframework.stereotype.Service
 
-@Service
-class PersonService {
-    fun getPersons(): List<Person> {
-        return emptyList()
-    }
+interface PersonService {
+    fun getPersons(): List<Person>
+    fun addPerson(person: Person)
+    fun getPersonByFirstname(firstname: String)
 }
